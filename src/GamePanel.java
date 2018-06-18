63e87ee9a2fef8543faf1ab9f97da4a9e0fc7482
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2018-06-18 07:56:40 -0400

    Initial commit

---
+++ src/GamePanel.java
@@ -0,0 +1,12 @@
+/*
+// File:             GamePanel.java
+// Created:          2018/06/18
+// Author:           danIv (Daniel Ivanovich)
+// Description:      Describe function here.
+*/
+
+public class GamePanel {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Window.java
@@ -0,0 +1,12 @@
+/*
+// File:             Window.java
+// Created:          2018/06/18
+// Author:           danIv (Daniel Ivanovich)
+// Description:      Describe function here.
+*/
+
+public class Window {
+    public static void main(String[] args) {
+
+    }
+}
